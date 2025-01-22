7813bf47d3a228d44e6b93ec6e08bf8003a20224
Darianna-dev <dariannagallo@gmail.com>
2025-01-22 01:15:42 +0000

    Se crea la funcion agegarAmigo

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-const input = document.querySelector("#amigo");
-let nombre = input.value;
-let nombreAmigo = nombre.trim().toLowerCase();
+// const input = document.querySelector("#amigo");
+// let nombre = input.value.trim().toLowerCase();
+// let nombreSinEspacios = nombre.trim().toLowerCase();
 
+// Expresión Regular para validar el nombre del amigo sin espacios y mímino 3 caracteres       ,;;
 const regex =
     /^(?!\s*$)[A-Za-zÁÉÍÓÚáéíóúñÑüÜ]{3,}(\s[A-Za-zÁÉÍÓÚáéíóúñÑüÜ]+)*$/;
 
@@ -17,4 +18,21 @@ function validarNombre(nombre) {
     }
 }
 
-function agregarAmigo() {}
+function agregarAmigo() {
+    try {
+        let nombre = input.value.trim().toLowerCase();
+        let nombreAmigo = validarNombre(nombre);
+        if (nombreAmigo) {
+            // Si el nombre es válido, agregar el amigo a la lista
+            let listaAmigos = document.querySelector("#listaAmigos");
+            let elemento = document.createElement("li");
+            elemento.innerHTML = nombre;
+            listaAmigos.appendChild(elemento);
+        } else {
+            // Si el nombre no es válido, mostrar un mensaje de error
+            alert("Nombre inválido");
+        }
+    } catch (error) {
+        console.error("Error al agregar amigo:", error);
+    }
+}
