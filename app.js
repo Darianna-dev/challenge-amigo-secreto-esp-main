1c9dae1c54ab61c201e99da0ca35cd0c61982972
Darianna-dev <dariannagallo@gmail.com>
2025-01-22 03:15:58 +0000

    se crea la función limpiar input, y se agrega el evento keydown

--- app.js
+++ app.js
@@ -1,38 +1,60 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-// const input = document.querySelector("#amigo");
-// let nombre = input.value.trim().toLowerCase();
-// let nombreSinEspacios = nombre.trim().toLowerCase();
+const listaAmigos = document.querySelector('#listaAmigos');
+const input = document.querySelector('#amigo');
+const arrayAmigos = [];
 
-// Expresión Regular para validar el nombre del amigo sin espacios y mímino 3 caracteres       ,;;
+// Expresión Regular para validar el nombre del amigo sin espacios y mímino 3 caracteres
 const regex =
     /^(?!\s*$)[A-Za-zÁÉÍÓÚáéíóúñÑüÜ]{3,}(\s[A-Za-zÁÉÍÓÚáéíóúñÑüÜ]+)*$/;
 
 function validarNombre(nombre) {
     if (regex.test(nombre)) {
-        console.log("Nombre válido");
+        // console.log('Nombre válido');
         return true;
     } else {
-        console.log("Nombre inválido");
+        // alert('Nombre inválido');
+        limpiarInput();
         return false;
     }
 }
 
 function agregarAmigo() {
     try {
-        let nombre = input.value.trim().toLowerCase();
+        let nombre = input.value.trim().toUpperCase();
         let nombreAmigo = validarNombre(nombre);
+        // Si el nombre es válido, agregar el amigo a la lista
         if (nombreAmigo) {
-            // Si el nombre es válido, agregar el amigo a la lista
-            let listaAmigos = document.querySelector("#listaAmigos");
-            let elemento = document.createElement("li");
-            elemento.innerHTML = nombre;
-            listaAmigos.appendChild(elemento);
+            if (arrayAmigos.includes(nombre)) {
+                alert('El amigo ya se encuentra en la lista');
+                limpiarInput();
+            } else {
+                arrayAmigos.push(nombre);
+                console.log(arrayAmigos);
+                // let listaAmigos = document.querySelector('#listaAmigos');
+                let elemento = document.createElement('li');
+                elemento.textContent = nombre;
+                listaAmigos.appendChild(elemento);
+                limpiarInput();
+            }
         } else {
             // Si el nombre no es válido, mostrar un mensaje de error
-            alert("Nombre inválido");
+            alert('Nombre inválido');
+            input.focus();
         }
     } catch (error) {
-        console.error("Error al agregar amigo:", error);
+        console.error('Error al agregar amigo:', error);
     }
+}
+
+// al presionar Enter llama a funcion agregarAmigo
+input.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        agregarAmigo();
+    }
+});
+
+function limpiarInput() {
+    input.value = '';
+    input.focus();
 }
